a897f99d4ea9b6b23734cd83a9519a3352331963
Alan Shortis <alan.shortis@mac.com>
2016-04-18 15:06:16 +0100

    Added Digital Projects item type

--- items.php
+++ items.php
@@ -10,6 +10,13 @@ $items = array(
     ),
     array(
         'metadata' => array(
+            'name' => 'Digital Project',
+            'description' => "A link to and metadata for an external digital project."
+        ),
+        'elements' => array()
+    ),
+    array(
+        'metadata' => array(
             'name' => 'Embedded Video',
             'description' => 'A video hosted at an external location such as Youtube or Vimeo.'
         ),
