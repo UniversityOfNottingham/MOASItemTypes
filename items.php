8e5f2da22cec22e9aa788604e1f03d4ce38c669b
Alan Shortis <alan.shortis@mac.com>
2016-04-19 09:13:07 +0100

    Revert

--- items.php
+++ items.php
@@ -10,20 +10,6 @@ $items = array(
     ),
     array(
         'metadata' => array(
-            'name' => 'Digital Project',
-            'description' => "A link to and metadata for an external digital project."
-        ),
-        'elements' => array()
-    ),
-    array(
-        'metadata' => array(
-            'name' => 'Person',
-            'description' => "Information about a specific person."
-        ),
-        'elements' => array()
-    ),
-    array(
-        'metadata' => array(
             'name' => 'Embedded Video',
             'description' => 'A video hosted at an external location such as Youtube or Vimeo.'
         ),
